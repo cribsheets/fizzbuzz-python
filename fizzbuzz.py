bece216176f6a29b7d27ba967ca8c5bdb433bf9e
Krauser <krauser.ja@gmail.com>
2019-01-29 22:15:50 -0500

    the most bland of solution, yet also the most bland

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,24 +1,12 @@
-#!/usr/bin/env python3
+def fizzbuzz(value):
+    if value % 15 == 0 :
+        print(str(value) + " = Fizzbuzz!")
+    elif value % 3 == 0:
+        print(str(value) + " = Fizz!")
+    elif value % 5 == 0:
+        print(str(value) + " = Buzz!")
+    else:
+        print(str(value) + " is the most boring of numbers :'(")
 
-# we include the above line to let the shell
-# know what interpreter to use should we 
-# decide to make this file executable
-
-# the python interpreter will start executing
-# code wherever it sees it in the file, so your
-# solution could simply be the body of the file
-# however, the following line, if included, will
-# be executed if the current file (fizzbuzz.py)
-# is the file on which the python3 interpreter
-# was called.
-
-# you see this quite a bit in idiomatic python, so
-# we include it here to induce good habits
-
-if __name__ == '__main__':
-    # remember the rules:
-    # - if the number is divisible by 3, print 'fizz'
-    # - if the number is divisible by 5, print 'buzz'
-    # - if the number is divisible by 3 AND 5, print 'fizzbuzz'
-    # - otherwise, print the number
-    print("write a solution here!")
+for value in range(1, 101):
+    fizzbuzz(value)
